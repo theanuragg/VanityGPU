296f35d677818e47a960426d94bbcd28e8499f59
anurag <anurags9693@gmail.com>
2026-01-01 01:34:07 +0530

    add handler fn for create job

--- api/src/handlers/create_job.rs
+++ api/src/handlers/create_job.rs
@@ -16,7 +16,28 @@ pub async fn create_job(
     state: web::Data<AppState>,
 ) -> Result<HttpResponse, ApiError> {
     verify_solana_pubkey(&payload.solana_pubkey)?;
-
-    //todo
-    Ok(HttpResponse::Ok().json("Job created successfully"))
+    
+    let job = Job {
+        id: Uuid::new_v4(),
+        created_at: Utc::now().naive_utc(),
+        solana_pubkey: payload.solana_pubkey.clone(),
+        desired_prefix: payload.desired_prefix.clone(),
+        desired_suffix: payload.desired_suffix.clone(),
+        result_db_url: payload.result_db_url.clone(),
+        result_table: payload.result_table.clone(),
+        webhook_url: payload.webhook_url.clone(),
+        
+        status: "pending".info(),
+        attempts: 10,
+        
+        matched_address: None,
+        matched_private_key: None,
+        worker_id: None,
+        attestations: None,
+    };
+    
+    insert_job(&state.db, &job).await?;
+    enqueue_job( &job).await?;
+    
+    Ok(HttpResponse::Ok().json(job.id))
 }
