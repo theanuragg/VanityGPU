68a7d4ba16250920049b886918b981b0984ce344
anurag <anurags9693@gmail.com>
2025-12-31 22:26:01 +0530

    add actix_web diesel_cli

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer};
+
+#[get("/health")]
+async fn health() -> HttpResponse {
+    HttpResponse::Ok().body("OK")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
