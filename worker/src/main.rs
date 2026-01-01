f33705571e7a1fc7eda8e5ee82a3faf663ef755c
anurag <anurags9693@gmail.com>
2026-01-01 23:52:49 +0530

    refactor the code ass redis fetching

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,6 +1,28 @@
+mod job;
+mod gpu;
+
+use std::str::from_boxed_utf8_unchecked;
+
+use gpu::VanityEngine;
+use gpu::cpu::CpuVanityEngine;
+use job::WorkerJob;
+
 fn main() {
-    loop{
-        let job = fetch_job_from_queue();
-        run_job(job);
+    let job = fetch_job_from_redis(); // todo add fetch job from redis to excute
+    
+    println!("Job received: {:?}", job.job_id);
+    
+    let engine =  CpuVanityEngine::new();
+    
+    let matches = engine.search(
+        &job.desired_prefix,
+        job.desired_suffix.as_deref(),
+        10,
+    );
+    for m in matches{
+        print!("match found: {}", m.pubkey.to_string());
+        
     }
+    
+    //todo: add  encryption seeds and sign attestation callback API
 }
