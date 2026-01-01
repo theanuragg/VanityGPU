e5c46a0de5b00bd6776f8feff26fffeb622c7086
anurag <anurags9693@gmail.com>
2026-01-01 23:32:50 +0530

    add trait

--- worker/src/gpu/mod.rs
+++ worker/src/gpu/mod.rs
@@ -1,2 +1,15 @@
-pub use cpu::*;
-pub mod cpu;
+use solana_sdk::pubkey::Pubkey;
+
+pub struct VanityMatch {
+    pub pubkey: Pubkey,
+    pub seed: [u8; 32],
+}
+
+pub trait VanityEngine {
+    fn search(
+        &self,
+        prefix: &str,
+        suffix: Option<&str>,
+        max_results: usize,
+    ) -> Vec<VanityMatch>;
+}
