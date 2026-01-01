148cd63523cc7864da75649910b1880351fce2c4
anurag <anurags9693@gmail.com>
2026-01-01 23:28:57 +0530

    refactor the worker job types

--- worker/src/job.rs
+++ worker/src/job.rs
@@ -1,5 +1,10 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
 pub struct VerifyJob {
     pub job_id: String,
     pub desired_prefix: String,
     pub desired_suffix: Option<String>,
+    pub user_encryption_pubkey: String,
+    pub webhook_url: String,
 }
