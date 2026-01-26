235eb6c2815bcd51d5337ccb175740e9a8dfb252
anurag <anurags9693@gmail.com>
2026-01-26 11:05:27 +0530

    Add Job model and NewJob struct: Introduce Job struct with Selectable trait and a new NewJob struct for job creation, enhancing database interaction capabilities.

--- api/src/models/job.rs
+++ api/src/models/job.rs
@@ -1,8 +1,9 @@
-use chrono::NaiveDataTime;
+use crate::schema::jobs;
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
 use uuid::Uuid;
 
-#[derive(Queryable, Insertable)]
+#[derive(Queryable, Selectable, Insertable, Debug)]
 #[diesel(table_name = jobs)]
 pub struct Job {
     pub id: Uuid,
@@ -19,4 +20,17 @@ pub struct Job {
     pub encrypted_private_key: Option<Vec<u8>>,
     pub worker_id: Option<String>,
     pub attestation_hash: Option<String>,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = jobs)]
+pub struct NewJob {
+    pub id: Uuid,
+    pub solana_pubkey: String,
+    pub desired_prefix: String,
+    pub desired_suffix: Option<String>,
+    pub result_db_url: String,
+    pub result_table: String,
+    pub webhook_url: String,
+    pub status: String,
 }
